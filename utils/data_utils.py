b7edd39b8bef222d9b8aa0aa67e41cbca1662e99
jethrocsau <jethrocsa@gmail.com>
2025-04-23 23:29:34 +0800

    update load data utils

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,14 +1,37 @@
+import os
+
 import numpy as np
 import pandas as pd
-import os
+import torch.nn.functional as F
 from ogb.nodeproppred import DglNodePropPredDataset
+from torch import Tensor
+from transformers import AutoModel, AutoTokenizer
 
 dataset_names = ['ogbn-mag','ogbn-arxiv']
 cwd = os.dir(os.path.dir(__file__))
 
-def load_ogb_dataset(name, root_dir):
+
+def load_ogb_dataset(name):
     """
     Load  OGB dataset.
+    Input: dataset name
+    Output: graph, labels, train_idx, valid_idx, test_idx
     """
 
-    return 0 
+    data_dir = os.path.join(cwd, 'data')
+    if not os.path.exist(data_dir):
+        os.makedirs(data_dir)
+
+    if name not in dataset_names:
+        raise ValueError(f"Dataset {name} not supported. Supported datasets are: {dataset_names}")
+    else:
+        dataset = DglNodePropPredDataset(name, root=os.path.join(cwd, 'data'))
+        split_idx = dataset.get_idx_split()
+        train_idx, valid_idx, test_idx = split_idx["train"], split_idx["valid"], split_idx["test"]
+        graph, label = dataset[0]
+        return graph, label, train_idx, valid_idx, test_idx
+
+
+
+
+
