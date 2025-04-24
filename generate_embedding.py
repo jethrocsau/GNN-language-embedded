aff5af8f40c1b7aeb211481b34a60ff48a321f16
jethrocsau <jethrocsa@gmail.com>
2025-04-24 15:23:42 +0800

    updated data_utils

---
+++ generate_embedding.py
@@ -0,0 +1,22 @@
+from data_utils import *
+
+'''
+args:
+dataset = 'ogbn-arxiv',
+model = 'graphmae',
+use_cfg = True,
+device = device,
+load_model = True,
+load_model_path = model_path,
+data_dir = os.path.join(cwd, 'data'),
+feat_type = 'e5_float16',
+use_cfg_path = os.path.join(cwd, 'ref', 'config','GraphMAE_configs.yml'),
+prob_num_workers = 4,
+moe = True,
+moe_use_linear = True,
+top_k = 1,
+num_expert = 4,
+hiddenhidden_size_times = 1,
+moe_layer = 0,
+decoder_no_moe = True
+'''
